d6151cdb717936b52162160c19df64b83556d3f2
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-07-02 16:19:42 +0200

    feat(dashboard): Now only uses polars, dropped pandas

--- dashboard.py
+++ dashboard.py
@@ -1,62 +1,38 @@
 import streamlit as st
 from datetime import datetime, timedelta
 
-try:
-    # raise ImportError
-    import polars as pl
-except ImportError:
-    pl = None
-    import pandas as pd
+import polars as pl
+
 # --- Load data ---
 st.set_page_config(layout='wide')
 
 
 @st.cache_data
 def load_rainfall():
-    if pl:
-        return pl.scan_parquet('rainfall.parquet')
-    else:
-        return pd.read_parquet('rainfall.parquet')
+    return pl.scan_parquet('rainfall.parquet').rename(
+        {'reference_timestamp': 'Time', 'rre150h0': 'Rainfall', 'station_name': 'Station'}
+    )
 
 
 @st.cache_data
 def load_metrics():
-    if pl:
-        return pl.read_parquet('metrics.parquet')
-    else:
-        return pd.read_parquet('metrics.parquet')
+    return pl.scan_parquet('metrics.parquet').rename(
+        {'rre150h0': 'Rainfall', 'station_name': 'Station', 'aggr_period_days': 'Time Period'}
+    )
 
 
 @st.cache_data
 def load_meta_stations():
-    if pl:
-        # meta_precip = pl.read_csv('ogd-smn-precip_meta_stations.csv', encoding='ISO-8859-1', separator=';')
-        # meta_weather = pl.read_csv('ogd-smn_meta_stations.csv', encoding='ISO-8859-1', separator=';', schema=meta_precip.schema)
-        return pl.scan_parquet('meta_stations.parquet')
-    else:
-        pass
+    return pl.scan_parquet('meta_stations.parquet').rename({'station_abbr': 'Station'})
 
 
 meta = load_meta_stations()
-rainfall = (
-    load_rainfall()
-    .join(meta.select(['station_abbr', 'station_name']).rename({'station_abbr': 'Station'}), on=['Station'])
-    .select(
-        [  # Replace Station with station_name
-            pl.exclude('Station')  # Select all other columns except 'Station'
-        ]
-    )
-    .rename({'station_name': 'Station'})
-)
+rainfall = load_rainfall()
 
 metrics = load_metrics()
 st.title('MeteoFungi')
 st.area_chart(
-    data=(
-        rainfall.filter(pl.col('Time') >= (datetime.now() - timedelta(days=7)))
-        if pl
-        else rainfall.loc[rainfall.Time >= (datetime.now() - timedelta(days=7))]
-    ),
+    data=rainfall.filter(pl.col('Time') >= (datetime.now() - timedelta(days=7))),
     x='Time',
     y='Rainfall',
     color='Station',
@@ -64,20 +40,19 @@ st.area_chart(
     y_label='Rainfall (mm)',
 )
 
+station_name_list = metrics.unique(subset=['Station']).sort('Station').select('Station').collect().to_series().to_list()
+
 st.subheader('3-Day Rainfall Sum (mm)')
 a, b, c, d, e = st.columns(5)
 for col, station in zip(
     [a, b, c, d, e],
-    (sorted(metrics.unique(subset=['Station']).get_column('Station').to_list()) if pl else rainfall.Station.unique()),
+    station_name_list,
 ):
     val = round(
-        (
-            metrics.filter((pl.col('Station') == station) & (pl.col('aggr_period_days') == 7))
-            .select(pl.col('Rainfall'))
-            .item()
-            if pl
-            else metrics.loc[(metrics.Station == station) & (metrics.aggr_period_days == 7), 'Rainfall'].iloc[0]
-        ),
+        metrics.filter((pl.col('Station') == station) & (pl.col('Time Period') == 7))
+        .select(pl.col('Rainfall'))
+        .collect()
+        .item(),
         2,
     )
     if val < 5:
