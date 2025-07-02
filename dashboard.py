16999e597ac1f351c1c734db87ecbef84fa72e98
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2025-07-02 21:30:49 +0200

    dev (#4)
    * feat(data_preparation): Add polars for speed
    
    * Apply automatic changes
    
    * feat(data_preparation): remove profiling
    
    * feat(data_preparation): rename fields
    
    * feat(dashboard): load rainfall data lazily with polars
    
    * feat(dashboard): load metadata from parquet file
    
    * feat(dashboard): Now only uses polars, dropped pandas
    
    * feat(data_preparation): Now only uses polars, dropped pandas
    
    * deps(): Remove pandas from deps
    
    * data(): Update input data
    
    * fix(dashboard): Fixed wrong time period in rainfall sum
    
    * fix(data_preparation): Now includes most recent data too
    
    * deps(requirements): Update requirements.txt
    
    * reformat(data_preparation): Reformat with Ruff
    
    * feat(dashboard): create get_rainfall_emoji()
    
    * feat(dashboard): Change to rain intensity in mm/d + compare trend

--- dashboard.py
+++ dashboard.py
@@ -26,6 +26,19 @@ def load_meta_stations():
     return pl.scan_parquet('meta_stations.parquet').rename({'station_abbr': 'Station'})
 
 
+def get_rainfall_emoji(val):
+    if val < 1:
+        return '☀️'  # No rain
+    elif 1 <= val < 10:
+        return '🌦️'  # Light rain
+    elif 10 <= val < 20:
+        return '🌧️'  # Moderate rain
+    elif 20 <= val < 50:
+        return '🌊'  # Heavy rain
+    else:
+        return '🌧️🌊'  # Very heavy rain
+
+
 meta = load_meta_stations()
 rainfall = load_rainfall()
 
@@ -42,26 +55,30 @@ st.area_chart(
 
 station_name_list = metrics.unique(subset=['Station']).sort('Station').select('Station').collect().to_series().to_list()
 
-st.subheader('3-Day Rainfall Sum (mm)')
+st.subheader('3-Day Average (mm/d)')
+
 a, b, c, d, e = st.columns(5)
 for col, station in zip(
     [a, b, c, d, e],
     station_name_list,
 ):
-    val = round(
+    val = (
         metrics.filter((pl.col('Station') == station) & (pl.col('Time Period') == 3))
         .select(pl.col('Rainfall'))
         .collect()
-        .item(),
-        2,
+        .item()
+        / 3
     )
-    if val < 1:
-        emo = '☀️'
-    elif (val >= 1) & (val < 20):
-        emo = '🌦️'
-    elif (val >= 20) & (val < 50):
-        emo = '🌧️'
-    else:
-        emo = '🌊'
-    col.metric(label=station, value=str(val) + emo)
-st.info('Source: MeteoSwiss')
+
+    delta = val - (
+        metrics.filter((pl.col('Station') == station) & (pl.col('Time Period') == 14))
+        .select(pl.col('Rainfall'))
+        .collect()
+        .item()
+        / 14
+    )
+    col.metric(label=station, value=(str(round(val, 1)) + ' ' + get_rainfall_emoji(val)), delta=round(delta, 1))
+
+with st.expander('Further Information'):
+    st.text('Delta values indicate difference between 3-day average and 14-day average.')
+    st.info('Data Sources: MeteoSwiss')
--- data_preparation.py
+++ data_preparation.py
@@ -20,49 +20,57 @@ def generate_download_url(station, station_type):
 
 def load_weather(stations, metadata):
     rainfall_recent = pl.scan_csv(
-                [
-                    generate_download_url(station, station_type)
-                    for station, station_type in stations.items()
-                    if station_type == 'rainfall'
-                ],
-                separator=';',
-                try_parse_dates=True,
-            )
+        [
+            generate_download_url(station, station_type)
+            for station, station_type in stations.items()
+            if station_type == 'rainfall'
+        ],
+        separator=';',
+        try_parse_dates=True,
+    )
     rainfall_now = pl.scan_csv(
-                [
-                    generate_download_url(station, station_type).replace('_recent', '_now')
-                    for station, station_type in stations.items()
-                    if station_type == 'rainfall'
-                ],
-                separator=';',
-                try_parse_dates=True,
-                schema = rainfall_recent.schema
-            )
+        [
+            generate_download_url(station, station_type).replace('_recent', '_now')
+            for station, station_type in stations.items()
+            if station_type == 'rainfall'
+        ],
+        separator=';',
+        try_parse_dates=True,
+        schema=rainfall_recent.schema,
+    )
     weather_recent = pl.scan_csv(
-            [
-                generate_download_url(station, station_type)
-                for station, station_type in stations.items()
-                if station_type == 'weather'
-            ],
-            separator=';',
-            try_parse_dates=True,
-        )
+        [
+            generate_download_url(station, station_type)
+            for station, station_type in stations.items()
+            if station_type == 'weather'
+        ],
+        separator=';',
+        try_parse_dates=True,
+    )
     weather_now = pl.scan_csv(
-            [
-                generate_download_url(station, station_type).replace('_recent', '_now')
-                for station, station_type in stations.items()
-                if station_type == 'weather'
-            ],
-            separator=';',
-            try_parse_dates=True,
-            schema=weather_recent.schema
-        )
+        [
+            generate_download_url(station, station_type).replace('_recent', '_now')
+            for station, station_type in stations.items()
+            if station_type == 'weather'
+        ],
+        separator=';',
+        try_parse_dates=True,
+        schema=weather_recent.schema,
+    )
     rainfall = pl.concat(
-        [rainfall_recent.filter(pl.col('reference_timestamp') < rainfall_now.select('reference_timestamp').min().collect().item()), rainfall_now
+        [
+            rainfall_recent.filter(
+                pl.col('reference_timestamp') < rainfall_now.select('reference_timestamp').min().collect().item()
+            ),
+            rainfall_now,
         ]
     )
     weather = pl.concat(
-        [weather_recent.filter(pl.col('reference_timestamp') < weather_now.select('reference_timestamp').min().collect().item()), weather_now
+        [
+            weather_recent.filter(
+                pl.col('reference_timestamp') < weather_now.select('reference_timestamp').min().collect().item()
+            ),
+            weather_now,
         ],
     )
     return (
