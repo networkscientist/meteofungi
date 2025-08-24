50d1d351e21f366c83b2aad4435f68aa96941e1f
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-08-24 23:17:39 +0200

    refactor(data_preparation): Use less resources

--- data_preparation.py
+++ data_preparation.py
@@ -1,18 +1,90 @@
+import polars.selectors as cs
 import polars as pl
 
+from pathlib import Path
 
-DTYPE_DICT = {
-    'Integer': pl.Int32,
-    'Float': pl.Float32,
-}
+DTYPE_DICT = {'Integer': pl.Int16, 'Float': pl.Float32, 'String': pl.String}
+
+METEO_CSV_ENCODING = 'ISO-8859-1'
 
 
 def load_meta_stations():
-    return pl.scan_parquet('meta_stations.parquet')
+    # if Path('meta_stations.parquet').exists():
+    #     return pl.scan_parquet('meta_stations.parquet')
+    cols_to_keep = ['station_' + x for x in 'abbr name canton type_de type_en dataowner data_since height_masl height_barometer_masl coordinates_lv95_east coordinates_lv95_north coordinates_wgs84_lat coordinates_wgs84_lon'.split()]
+    schem = {
+        'station_abbr': pl.String,
+        'station_name': pl.String,
+        'station_canton': pl.String,
+        'station_wigos_id': pl.String,
+        'station_type_de': pl.String,
+        'station_type_fr': pl.String,
+        'station_type_it': pl.String,
+        'station_type_en': pl.String,
+        'station_dataowner': pl.String,
+        'station_data_since': pl.String,
+        'station_height_masl': pl.Float64,
+        'station_height_barometer_masl': pl.Float64,
+        'station_coordinates_lv95_east': pl.Float64,
+        'station_coordinates_lv95_north': pl.Float64,
+        'station_coordinates_wgs84_lat': pl.Float64,
+        'station_coordinates_wgs84_lon': pl.Float64,
+        'station_exposition_de': pl.String,
+        'station_exposition_fr': pl.String,
+        'station_exposition_it': pl.String,
+        'station_exposition_en': pl.String,
+        'station_url_de': pl.String,
+        'station_url_fr': pl.String,
+        'station_url_it': pl.String,
+        'station_url_en': pl.String,
+    }
+    stations = (
+        pl.concat(
+            [
+                pl.read_csv(
+                    f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_stations.csv',
+                    encoding=METEO_CSV_ENCODING,
+                    separator=';',
+                    schema=schem,
+                    columns=cols_to_keep
+                )
+                for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
+            ]
+        )
+    )
+    stations.write_parquet('meta_stations.parquet')
+    return stations.lazy()
 
 
 def load_meta_params():
-    return pl.scan_parquet('meta_params.parquet')
+    schem = {
+        'parameter_shortname': pl.String,
+        'parameter_description_de': pl.String,
+        'parameter_description_fr': pl.String,
+        'parameter_description_it': pl.String,
+        'parameter_description_en': pl.String,
+        'parameter_group_de': pl.String,
+        'parameter_group_fr': pl.String,
+        'parameter_group_it': pl.String,
+        'parameter_group_en': pl.String,
+        'parameter_granularity': pl.String,
+        'parameter_decimals': pl.Int8,
+        'parameter_datatype': pl.String,
+        'parameter_unit': pl.String,
+    }
+    params = pl.concat(
+        [
+            pl.read_csv(
+                f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_parameters.csv',
+                encoding=METEO_CSV_ENCODING,
+                separator=';',
+                schema=schem,
+            )
+            for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
+        ]
+    ).drop([cs.ends_with('_fr'), cs.ends_with('_it')])
+    params.write_parquet('meta_parameters.parquet')
+    return params.lazy()
 
 
 schema_dict = {
