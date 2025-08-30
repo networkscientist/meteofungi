429a41f4e1d00a3d777e4d6a8463103dcaf6102b
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-08-30 20:07:08 +0200

    refactor(data_preparation): extract schemata and cols_to_keep into constants

--- src/meteofungi/data_preparation/data_preparation.py
+++ src/meteofungi/data_preparation/data_preparation.py
@@ -1,115 +1,120 @@
-import polars.selectors as cs
 import polars as pl
 from datetime import datetime, timedelta
 
 from pathlib import Path
 
-DATA_PATH = Path(__file__).resolve().parents[3].joinpath('data')
-DTYPE_DICT = {'Integer': pl.Int16, 'Float': pl.Float32, 'String': pl.String}
+DATA_PATH: Path = Path(__file__).resolve().parents[3].joinpath('data')
+DTYPE_DICT: dict = {'Integer': pl.Int16, 'Float': pl.Float32, 'String': pl.String}
 
 METEO_CSV_ENCODING: str = 'ISO-8859-1'
-
-
-def load_meta_stations() -> pl.LazyFrame:
-    cols_to_keep: list[str] = [
-        'station_' + x
-        for x in 'abbr name canton type_de type_en dataowner data_since height_masl height_barometer_masl coordinates_lv95_east coordinates_lv95_north coordinates_wgs84_lat coordinates_wgs84_lon'.split()
+META_FILE_PATH_DICT: dict[str, list[str]] = {
+    meta_type: [
+        f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_{meta_type}.csv'
+        for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
     ]
-    schem = {
-        'station_abbr': pl.String,
-        'station_name': pl.String,
-        'station_canton': pl.String,
-        'station_wigos_id': pl.String,
-        'station_type_de': pl.String,
-        'station_type_fr': pl.String,
-        'station_type_it': pl.String,
-        'station_type_en': pl.String,
-        'station_dataowner': pl.String,
-        'station_data_since': pl.String,
-        'station_height_masl': pl.Float64,
-        'station_height_barometer_masl': pl.Float64,
-        'station_coordinates_lv95_east': pl.Float64,
-        'station_coordinates_lv95_north': pl.Float64,
-        'station_coordinates_wgs84_lat': pl.Float64,
-        'station_coordinates_wgs84_lon': pl.Float64,
-        'station_exposition_de': pl.String,
-        'station_exposition_fr': pl.String,
-        'station_exposition_it': pl.String,
-        'station_exposition_en': pl.String,
-        'station_url_de': pl.String,
-        'station_url_fr': pl.String,
-        'station_url_it': pl.String,
-        'station_url_en': pl.String,
-    }
+    for meta_type in ['stations', 'parameters', 'datainventory']
+}
+
+SCHEMA_META_STATIONS: dict = {
+    'station_abbr': pl.String,
+    'station_name': pl.String,
+    'station_canton': pl.String,
+    'station_wigos_id': pl.String,
+    'station_type_de': pl.String,
+    'station_type_fr': pl.String,
+    'station_type_it': pl.String,
+    'station_type_en': pl.String,
+    'station_dataowner': pl.String,
+    'station_data_since': pl.String,
+    'station_height_masl': pl.Float64,
+    'station_height_barometer_masl': pl.Float64,
+    'station_coordinates_lv95_east': pl.Float64,
+    'station_coordinates_lv95_north': pl.Float64,
+    'station_coordinates_wgs84_lat': pl.Float64,
+    'station_coordinates_wgs84_lon': pl.Float64,
+    'station_exposition_de': pl.String,
+    'station_exposition_fr': pl.String,
+    'station_exposition_it': pl.String,
+    'station_exposition_en': pl.String,
+    'station_url_de': pl.String,
+    'station_url_fr': pl.String,
+    'station_url_it': pl.String,
+    'station_url_en': pl.String,
+}
+
+SCHEMA_META_PARAMETERS: dict = {
+    'parameter_shortname': pl.String,
+    'parameter_description_de': pl.String,
+    'parameter_description_fr': pl.String,
+    'parameter_description_it': pl.String,
+    'parameter_description_en': pl.String,
+    'parameter_group_de': pl.String,
+    'parameter_group_fr': pl.String,
+    'parameter_group_it': pl.String,
+    'parameter_group_en': pl.String,
+    'parameter_granularity': pl.String,
+    'parameter_decimals': pl.Int8,
+    'parameter_datatype': pl.String,
+    'parameter_unit': pl.String,
+}
+
+SCHEMA_META_DATAINVENTORY: dict = {
+    'station_abbr': pl.String,
+    'parameter_shortname': pl.String,
+    'meas_cat_nr': pl.Int8,
+    'data_since': pl.Datetime,
+    'data_till': pl.Datetime,
+    'owner': pl.String,
+}
+
+COLS_TO_KEEP_META_STATIONS: list[str] = [
+    'station_abbr',
+    'station_name',
+    'station_canton',
+    'station_type_de',
+    'station_type_en',
+    'station_dataowner',
+    'station_data_since',
+    'station_height_masl',
+    'station_height_barometer_masl',
+    'station_coordinates_lv95_east',
+    'station_coordinates_lv95_north',
+    'station_coordinates_wgs84_lat',
+    'station_coordinates_wgs84_lon',
+]
+
+COLS_TO_KEEP_META_PARAMETERS: list[str] = [
+    'parameter_datatype',
+    'parameter_decimals',
+    'parameter_description_de',
+    'parameter_description_en',
+    'parameter_granularity',
+    'parameter_group_de',
+    'parameter_group_en',
+    'parameter_shortname',
+    'parameter_unit',
+]
+
+COLS_TO_KEEP_META_DATAINVENTORY: list[str] = ['data_since', 'data_till', 'owner', 'parameter_shortname', 'station_abbr']
+
+
+def load_metadata(
+    meta_type: str, file_path_dict: dict[str, list[str]], meta_schema: dict, meta_cols_to_keep: list[str]
+) -> pl.LazyFrame:
     stations: pl.DataFrame = pl.concat(
         [
             pl.read_csv(
-                f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_stations.csv',
+                file_path,
                 encoding=METEO_CSV_ENCODING,
                 separator=';',
-                schema=schem,
-                columns=cols_to_keep,
+                schema=meta_schema,
+                columns=meta_cols_to_keep,
             )
-            for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
+            for file_path in file_path_dict[meta_type]
         ]
     )
     stations.write_parquet(Path(DATA_PATH, 'meta_stations.parquet'))
     return stations.lazy()
-
-
-def load_meta_params() -> pl.LazyFrame:
-    schem = {
-        'parameter_shortname': pl.String,
-        'parameter_description_de': pl.String,
-        'parameter_description_fr': pl.String,
-        'parameter_description_it': pl.String,
-        'parameter_description_en': pl.String,
-        'parameter_group_de': pl.String,
-        'parameter_group_fr': pl.String,
-        'parameter_group_it': pl.String,
-        'parameter_group_en': pl.String,
-        'parameter_granularity': pl.String,
-        'parameter_decimals': pl.Int8,
-        'parameter_datatype': pl.String,
-        'parameter_unit': pl.String,
-    }
-    params: pl.DataFrame = pl.concat(
-        [
-            pl.read_csv(
-                f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_parameters.csv',
-                encoding=METEO_CSV_ENCODING,
-                separator=';',
-                schema=schem,
-            )
-            for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
-        ]
-    ).drop([cs.ends_with('_fr'), cs.ends_with('_it')])
-    params.write_parquet(Path(DATA_PATH, 'meta_parameters.parquet'))
-    return params.lazy()
-
-
-def load_meta_datainventory() -> pl.LazyFrame:
-    schem = {
-        'station_abbr': pl.String,
-        'parameter_shortname': pl.String,
-        'meas_cat_nr': pl.Int8,
-        'data_since': pl.Datetime,
-        'data_till': pl.Datetime,
-        'owner': pl.String,
-    }
-    datainventory: pl.DataFrame = pl.concat(
-        [
-            pl.read_csv(
-                f'https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn{ogd_smn_prefix}/ogd-smn{meta_suffix}_meta_datainventory.csv',
-                encoding=METEO_CSV_ENCODING,
-                separator=';',
-                schema=schem,
-            )
-            for ogd_smn_prefix, meta_suffix in zip(['', '-precip', '-tower'], ['', '-precip', '-tower'])
-        ]
-    ).drop('meas_cat_nr')
-    datainventory.write_parquet(Path(DATA_PATH, 'meta_datainventory.parquet'))
-    return datainventory.lazy()
 
 
 def generate_download_url(station: str, station_type: str, timeframe: str) -> str:
@@ -126,7 +131,7 @@ def load_weather(metadata: pl.LazyFrame, schema_dict_lazyframe: dict) -> pl.Lazy
     stations: pl.DataFrame = (
         metadata.select('station_abbr', 'station_type_en').unique('station_abbr').sort('station_abbr').collect()
     )
-    kwargs_lazyframe = {'separator': ';', 'try_parse_dates': True, 'schema_overrides': schema_dict_lazyframe}
+    kwargs_lazyframe: dict = {'separator': ';', 'try_parse_dates': True, 'schema_overrides': schema_dict_lazyframe}
     station_series_precipitation: pl.Series = filter_stations_to_series(
         stations, station_type='Automatic precipitation stations'
     )
@@ -166,7 +171,7 @@ def load_weather(metadata: pl.LazyFrame, schema_dict_lazyframe: dict) -> pl.Lazy
 
 
 def create_rainfall_weather_lazyframes(
-    station_series: pl.Series, station_type: str, timeframe: str, kwargs_lazyframe
+    station_series: pl.Series, station_type: str, timeframe: str, kwargs_lazyframe: dict
 ) -> pl.LazyFrame:
     return pl.scan_csv(
         [generate_download_url(station, station_type, timeframe) for station in station_series],
@@ -181,15 +186,21 @@ def filter_stations_to_series(stations: pl.DataFrame, station_type: str) -> pl.S
 
 
 if __name__ == '__main__':
-    weather_schema_dict = {
+    weather_schema_dict: dict = {
         colname: DTYPE_DICT[datatype]
-        for colname, datatype in load_meta_params()
+        for colname, datatype in load_metadata(
+            'parameters', META_FILE_PATH_DICT, SCHEMA_META_PARAMETERS, COLS_TO_KEEP_META_PARAMETERS
+        )
         .select(pl.col('parameter_shortname'), pl.col('parameter_datatype'))
         .collect()
         .iter_rows()
     }
 
-    meta_stations: pl.LazyFrame = load_meta_stations()
-    meta_datainventory = load_meta_datainventory()
+    meta_stations: pl.LazyFrame = load_metadata(
+        'stations', META_FILE_PATH_DICT, SCHEMA_META_STATIONS, COLS_TO_KEEP_META_STATIONS
+    )
+    meta_datainventory: pl.LazyFrame = load_metadata(
+        'datainventory', META_FILE_PATH_DICT, SCHEMA_META_DATAINVENTORY, COLS_TO_KEEP_META_DATAINVENTORY
+    )
     weather_data: pl.LazyFrame = load_weather(meta_stations, schema_dict_lazyframe=weather_schema_dict)
     weather_data.sink_parquet(Path(DATA_PATH, 'weather_data.parquet'), compression='brotli', compression_level=11)
