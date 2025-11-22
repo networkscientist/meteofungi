7e2bfc32f8cc1089d9e52c41cc610ce0212881c0
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-11-22 17:37:46 +0100

    feat(dashboard): Change some LazyFrames to DataFrames

--- src/meteofungi/dashboard/dashboard_utils.py
+++ src/meteofungi/dashboard/dashboard_utils.py
@@ -19,31 +19,33 @@ from meteofungi.dashboard.constants import (
 
 
 @st.cache_data
-def load_metadata_to_frame(meta_type: str) -> pl.LazyFrame:
+def load_metadata_to_frame(meta_type: str) -> pl.DataFrame:
     """Load  metadata
 
     Returns
     -------
         Metadata in Polars LazyFrame
     """
-    return pl.scan_parquet(
+    return pl.read_parquet(
         Path(DATA_PATH, f'meta_{meta_type.lower()}.parquet')
     ).unique()
 
 
-def collect_meta_params_to_dicts(metadata):
+@st.cache_data
+def collect_meta_params_to_dicts(_metadata):
     rows: tuple[dict[str, Any]] = tuple(
-        metadata.collect().to_dicts(),
+        _metadata.to_dicts(),
     )
     return rows
 
 
-def create_meta_map(metadata):
+@st.cache_data
+def create_meta_map(_metadata):
     meta_map: dict = {
         r['parameter_shortname']: re.search(
             parameter_description_extraction_pattern, r['parameter_description_en']
         ).group()
-        for r in collect_meta_params_to_dicts(metadata)
+        for r in collect_meta_params_to_dicts(_metadata)
     }
     return meta_map
 
@@ -61,7 +63,7 @@ def create_stations_options_selected(station_name_list):
 @st.cache_data
 def create_metrics(
     _weather_data: pl.LazyFrame, time_periods: Mapping[int, datetime]
-) -> pl.LazyFrame:
+) -> pl.DataFrame:
     return pl.concat(
         [
             _weather_data.filter(pl.col('reference_timestamp') >= datetime_period)
@@ -74,7 +76,7 @@ def create_metrics(
             .with_columns(pl.lit(period).alias('time_period'))
             for period, datetime_period in time_periods.items()
         ]
-    )
+    ).collect()
 
 
 @st.cache_data
@@ -90,43 +92,39 @@ def create_station_name_list(_frame_with_stations: pl.LazyFrame) -> tuple[str, .
 
 
 @st.cache_data
-def create_station_frame_for_map(_frame_with_stations: pl.LazyFrame) -> pl.DataFrame:
-    return (
-        _frame_with_stations.with_columns(
-            pl.col('station_type_en').alias('Station Type'),
-            pl.col('station_abbr').alias('Short Code'),
-            Altitude=pl.col('station_height_masl')
-            .cast(pl.Int16)
-            .cast(pl.String)
-            .add(' m.a.s.l'),
-        )
-        .select(
-            pl.col(
-                (
-                    'Short Code',
-                    'Station Type',
-                    'station_name',
-                    'station_coordinates_wgs84_lat',
-                    'station_coordinates_wgs84_lon',
-                    'Altitude',
-                )
+def create_station_frame_for_map(_frame_with_stations: pl.DataFrame) -> pl.DataFrame:
+    return _frame_with_stations.with_columns(
+        pl.col('station_type_en').alias('Station Type'),
+        pl.col('station_abbr').alias('Short Code'),
+        Altitude=pl.col('station_height_masl')
+        .cast(pl.Int16)
+        .cast(pl.String)
+        .add(' m.a.s.l'),
+    ).select(
+        pl.col(
+            (
+                'Short Code',
+                'Station Type',
+                'station_name',
+                'station_coordinates_wgs84_lat',
+                'station_coordinates_wgs84_lon',
+                'Altitude',
             )
         )
-        .collect()
     )
 
 
 @st.cache_data
-def load_weather_data() -> pl.LazyFrame:
-    return pl.scan_parquet(Path(DATA_PATH, 'weather_data.parquet')).with_columns(
+def load_weather_data() -> pl.DataFrame:
+    return pl.read_parquet(Path(DATA_PATH, 'weather_data.parquet')).with_columns(
         pl.col('reference_timestamp').dt.replace_time_zone(
             TIMEZONE_SWITZERLAND_STRING, non_existent='null'
         )
     )
 
 
-META_PARAMETERS: pl.LazyFrame = load_metadata_to_frame('parameters')
-META_STATIONS: pl.LazyFrame = load_metadata_to_frame('stations')
+META_PARAMETERS: pl.DataFrame = load_metadata_to_frame('parameters')
+META_STATIONS: pl.DataFrame = load_metadata_to_frame('stations')
 METRICS_NAMES_DICT: dict[str, str] = {
     m: create_meta_map(META_PARAMETERS).get(m, '') for m in METRICS_STRINGS
 }
