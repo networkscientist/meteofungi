72ef303ab56d30a10f8cc73539cf9cb9290f3187
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-11-23 19:36:08 +0100

    feat(dashboard): Rename parameters to draw_map()

--- src/meteofungi/dashboard/dashboard_map.py
+++ src/meteofungi/dashboard/dashboard_map.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import polars as pl
 import streamlit as st
 from plotly import express as px
 from plotly.graph_objs import Figure
@@ -11,12 +12,18 @@ from meteofungi.dashboard.dashboard_utils import (
 )
 
 
-def draw_map(metrics, param, period):
-    station_frame_for_map = create_station_frame_for_map(META_STATIONS, metrics, period)
-    scatter_map_kwargs: dict[str, str | dict[str, bool] | list[str | Any] | int] = {
+def draw_map(metrics: pl.LazyFrame, param_short_code: str, time_period: int):
+    if not time_period:
+        time_period = 7
+    station_frame_for_map: pl.LazyFrame = create_station_frame_for_map(
+        META_STATIONS, metrics, time_period
+    )
+    scatter_map_kwargs: dict[
+        str, str | dict[str, bool] | list[str | Any] | int | None
+    ] = {
         'lat': 'station_coordinates_wgs84_lat',
         'lon': 'station_coordinates_wgs84_lon',
-        'color': (WEATHER_SHORT_LABEL_DICT.get(param, 'Station Type')),
+        'color': (WEATHER_SHORT_LABEL_DICT.get(param_short_code, 'Station Type')),
         'hover_name': 'station_name',
         'hover_data': {
             'Station Type': False,
@@ -29,10 +36,10 @@ def draw_map(metrics, param, period):
         'size_max': 15,
         'zoom': 6,
         'map_style': 'carto-positron',
-        'title': (WEATHER_SHORT_LABEL_DICT.get(param, 'Stations')),
+        'title': (WEATHER_SHORT_LABEL_DICT.get(param_short_code, 'Stations')),
         'subtitle': (
-            f'Over the last {period} days'
-            if param in WEATHER_SHORT_LABEL_DICT
+            f'Over the last {time_period} days'
+            if param_short_code in WEATHER_SHORT_LABEL_DICT
             else None
         ),
     }
